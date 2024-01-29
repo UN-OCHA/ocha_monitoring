9be99b4911092584e6ffc2f18e81f3786622be9f
Peter Droogmans (attiks) <peter@attiks.com>
2024-01-29 13:46:17 +0100

    chore: add un_date, gtm_barebones

--- src/Plugin/monitoring/SensorPlugin/OchaGoogleTagSensorPlugin.php
+++ src/Plugin/monitoring/SensorPlugin/OchaGoogleTagSensorPlugin.php
@@ -16,7 +16,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   id = "ocha_google_tag",
  *   label = @Translation("Google tag"),
  *   description = @Translation("Monitors google tag."),
- *   provider = "google_tag",
  *   addable = FALSE
  * )
  */
