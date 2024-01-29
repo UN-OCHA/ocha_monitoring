41882b00c557545a659ad1a2bd61a6c0e175b83e
Peter Droogmans (attiks) <peter@attiks.com>
2024-01-29 16:14:40 +0100

    Use post update and deploy to enable sensors

---
+++ ocha_monitoring.deploy.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Monitoring file.
+ */
+
+/**
+ * Re-add al config.
+ */
+function ocha_monitoring_deploy_v106(&$sandbox) {
+  ocha_monitoring_enforce_monitors();
+}
--- ocha_monitoring.post_update.php
+++ ocha_monitoring.post_update.php
@@ -8,6 +8,13 @@
 /**
  * Re-add al config.
  */
-function ocha_monitoring_hook_post_update_v105(&$sandbox) {
+function ocha_monitoring_post_update_v105(&$sandbox) {
+  ocha_monitoring_enforce_monitors();
+}
+
+/**
+ * Re-add al config.
+ */
+function ocha_monitoring_post_update_v106(&$sandbox) {
   ocha_monitoring_enforce_monitors();
 }
