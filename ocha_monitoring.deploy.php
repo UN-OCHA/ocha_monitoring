96a5fb2f203caabe0e887b4840d8c562627d6942
Peter Droogmans (attiks) <peter@attiks.com>
2024-02-01 16:28:03 +0100

    Add sensor for self

--- ocha_monitoring.deploy.php
+++ ocha_monitoring.deploy.php
@@ -11,3 +11,10 @@
 function ocha_monitoring_deploy_v106(&$sandbox) {
   ocha_monitoring_enforce_monitors();
 }
+
+/**
+ * Re-add al config.
+ */
+function ocha_monitoring_deploy_v1011(&$sandbox) {
+  ocha_monitoring_enforce_monitors();
+}
