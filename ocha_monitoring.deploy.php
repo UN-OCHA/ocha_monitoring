f2b07c2d70d9d5d8af9c6c1850e1f69c1ab360f5
Peter Droogmans (attiks) <peter@attiks.com>
2024-02-02 16:29:36 +0100

    Force enable/disable sensors

--- ocha_monitoring.deploy.php
+++ ocha_monitoring.deploy.php
@@ -8,20 +8,6 @@
 /**
  * Re-add al config.
  */
-function ocha_monitoring_deploy_v106(&$sandbox) {
-  ocha_monitoring_enforce_monitors();
-}
-
-/**
- * Re-add al config.
- */
-function ocha_monitoring_deploy_v1011(&$sandbox) {
-  ocha_monitoring_enforce_monitors();
-}
-
-/**
- * Re-add al config.
- */
-function ocha_monitoring_deploy_v1012(&$sandbox) {
+function ocha_monitoring_deploy_v1013(&$sandbox) {
   ocha_monitoring_enforce_monitors();
 }
