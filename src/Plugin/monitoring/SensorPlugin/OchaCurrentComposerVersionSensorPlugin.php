68858310c43244fe9b9da872bda5a3c0be5caa3a
Peter Lieverdink <lieverdink@reliefweb.int>
2024-02-09 13:08:49 +1100

    feat: Report the version of composer that is first in $PATH.
    This allows us to catch projects that have an old version of composer pinned in the lock file.
    
    Refs: OPS-10076

---
+++ src/Plugin/monitoring/SensorPlugin/OchaCurrentComposerVersionSensorPlugin.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\ocha_monitoring\Plugin\monitoring\SensorPlugin;
+
+use Drupal\monitoring\Result\SensorResultInterface;
+use Drupal\monitoring\SensorPlugin\SensorPluginBase;
+
+/**
+ * Monitors current Composer version.
+ *
+ * @SensorPlugin(
+ *   id = "ocha_current_composer_version",
+ *   label = @Translation("Current Composer version"),
+ *   description = @Translation("Current Composer version."),
+ *   addable = FALSE
+ * )
+ *
+ * Based on ocha_current_php_version.
+ */
+class OchaCurrentComposerVersionSensorPlugin extends SensorPluginBase {
+
+  /**
+   * Major version.
+   */
+  private $major = 0;
+
+  /**
+   * Minor version.
+   */
+  private $minor = 0;
+
+  /**
+   * Release version.
+   */
+  private $release = 0;
+
+  /**
+   * Release date.
+   */
+  private $datetime = '';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function runSensor(SensorResultInterface $result) {
+    if ($this->getComposerVersion() == TRUE) {
+      $result->setValue($this->major . '.' . $this->minor . '.' . $this->release);
+      $result->setMessage('Composer ' . $this->major . '.' . $this->minor . '.' . $this->release . ' ' . $this->datetime);
+      $result->setStatus(SensorResultInterface::STATUS_INFO);
+    }
+    else {
+      $result->setStatus(SensorResultInterface::STATUS_ERROR);
+    }
+  }
+
+  /**
+   * Run composer -V and parse the output to extract the version.
+   *
+   * @return boolean.
+   */
+  private function getComposerVersion() {
+    $desc = [
+      0 => ["pipe", "r"],
+      1 => ["pipe", "w"],
+      2 => ["pipe", "w"],
+    ];
+
+    // Run compose rusing proc_open, which is allegedly safest.
+    $composer = proc_open(['composer', '-V'], $descriptorspec, $pipes);
+
+    $composer_version_string = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+
+    // Return null on error, so the plugin can set error status.
+    $ret = proc_close($composer);
+    if ($ret !== 0) {
+      return FALSE;
+    }
+
+    // Extract the version string using sscanf().
+    // This will break if the string format changes.
+    // "Composer version 2.6.6 2023-12-08 18:32:26"
+    $ret = sscanf($version_output, "Composer version %d.%d.%d %s %s");
+
+    // We should have an array with 5 values.
+    if (count($ret)) !== 5) {
+        return FALSE;
+    }
+
+    // Set the versions.
+    $this->major   = $ret[0];
+    $this->minor   = $ret[1];
+    $this->release = $ret[2];
+
+    // And the timestamp.
+    $this->datetime = $ret[3] . ' ' . $ret[4];
+
+    return TRUE;
+  }
+
+}
