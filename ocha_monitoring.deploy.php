f728211053c9c9b57345fb0df6474568e6ae6784
Peter Droogmans (attiks) <peter@attiks.com>
2024-02-09 11:22:47 +0100

    chore: add config

--- ocha_monitoring.deploy.php
+++ ocha_monitoring.deploy.php
@@ -11,3 +11,10 @@
 function ocha_monitoring_deploy_v1013(&$sandbox) {
   ocha_monitoring_enforce_monitors();
 }
+
+/**
+ * Re-add al config.
+ */
+function ocha_monitoring_deploy_v1014(&$sandbox) {
+  ocha_monitoring_enforce_monitors();
+}
